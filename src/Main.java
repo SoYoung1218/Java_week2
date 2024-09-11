702a937b479fe407968682d22e1a92ecaceac9e4
SoyYoung1218 <ehdudala1930@gmail.com>
2024-09-11 17:22:33 +0900

    sy

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+
+/*public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Main2 {
+    public void main(String[] args) {
+        System.out.println("Hello, to world!");
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        System.out.println("Hello, World1");
+        System.out.println("Hello, World2");
+    }
+}*/
+/*public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        System.out.println("Hello, World1");
+        System.out.println("Hello, World2");
+    }
+}*/
+/*public class Main {
+
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 100;
+        boolean c = true;
+        char d = 'c';
+        string e = "e" ;
+
+        System.out.println(a); //안녕 하세요!
+        System.out.println(b); //안녕 하세요!
+        System.out.println(c); //안녕 하세요!
+    }
+}*/
+public class Main {
+
+    public static void main(String[] args) {
+        char letter = 'A';
+        int ascllvalue = letter;
+        System.out.println("The ASCII value of" + lketter + is"")
+    }
+
